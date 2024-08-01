340d24396be0bbd3548b5f2788edb8676722fc8f
Hasitha Rukmal <rukmal7@yahoo.com>
2024-08-01 22:51:33 +0530

    updated seeders

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const path = require("path");
+
+// Load .env file from the project root
+dotenv.config({ path: path.join(__dirname, "./../../../.env") });
+
+// Load .env file from service root
+// dotenv.config();
 
 const roleSeeder = require("./role.seeder");
 const userSeeder = require("./user.seeder");
@@ -8,14 +15,14 @@ const profileSeeder = require("./profile.seeder");
 // db connection
 const dbName =
   process.env.NODE_ENV === "test"
-    ? process.env.dbTesting
+    ? process.env.DB_TESTING
     : process.env.NODE_ENV === "production"
-    ? process.env.dbProduction
-    : process.env.dbDevelopment;
+    ? process.env.DB_PRODUCTION
+    : process.env.DB_DEVELOPMENT;
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.connectionString}/${dbName}?retryWrites=true&w=majority&appName=${process.env.appName}`
+    `mongodb+srv://${process.env.DB_CONNECTION_STRING}/${dbName}?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`
   )
   .then(() => {
     console.log("Connected to the databse");
